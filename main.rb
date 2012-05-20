765239e99f51a63595cf05fa58d8ddeb2d8f55f5
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-05-20 15:21:56 -0500

    More examples for evaluation in context

--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@ require "./spea2"
 require "./eval_context"
 
 module Functions
-  def z
-    i + k*m*v+sfcv[0]
+  def z1
+    i + k*m*v + sfcv[0]
+  end
+
+  def z2
+    incrementolineal["interseccion"].inject{|sum, i| sum+i}
   end
 end
 
 context = EvalContext.new(Functions)
 context.load_variables("info.json")
 spea2_instance = Spea2.new(context)
-puts spea2_instance.context.z
+puts spea2_instance.context.z1
+puts spea2_instance.context.z2
