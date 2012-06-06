e1ce32f5d5779bc11a8106fa4c2c07994df6e992
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-06 01:06:59 -0500

    Load Restrictions and Solutions modules in main

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require "./spea2"
 require "./eval_context"
 require "./functions"
+require "./restrictions"
+require "./solutions"
 
-context = EvalContext.new(Functions)
+context = EvalContext.new(Functions, Solutions, Restrictions)
 context.load_variables("info.json")
 spea2_instance = Spea2.new(context)
 puts spea2_instance.context.z1
