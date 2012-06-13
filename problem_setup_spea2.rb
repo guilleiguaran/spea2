c8e34e56ee970dae175691cd4635ae67e4b8e925
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-13 15:18:00 -0500

    Ok, last commit, now for real :trollface:

--- problem_setup_spea2.rb
+++ problem_setup_spea2.rb
@@ -4,12 +4,38 @@ require "./functions"
 require "./restrictions"
 require "./solutions"
 
+def print_solution(context, vector, n)
+  context.generate_solution(vector)
+  csv = CSV.open("solution#{n}.csv", "wb")
+  csv << ["g"]
+  context.g.each do |gi|
+    gi.each do |ex|
+      csv << ex
+    end
+  end
+  csv << [""]
+  csv << ["q"]
+  context.q.each do |qi|
+    qi.each do |ex|
+      ex.each do |exi|
+        csv << exi
+      end
+    end
+  end
+  csv << [""]
+  csv << ["c"]
+  context.c.each do |ci|
+    csv << ci
+  end
+  csv.close
+end
+
 context = EvalContext.new(Functions, Solutions, Restrictions)
 context.load_variables("info.json")
 options = {
   :problem_size => 2,
   :search_space => Array.new(2) {|i| [0, 10] },
-  :max_gens => 50,
+  :max_gens => 1,
   :pop_size => 80,
   :archive_size => 40,
   :p_cross => 0.98
@@ -22,6 +48,7 @@ options = {
   archive = spea2_instance.search
   archive.each do |x|
     csv << x[:objectives].join(",").split(",")
+    print_solution(context, x[:vector], n)
   end
   csv.close
 end
--- spea2.rb
+++ spea2.rb
@@ -154,7 +154,6 @@ class Spea2
   end
 
   def search
-    csv = CSV.open("pareto#{context.n}.csv", "wb")
     pop = Array.new(pop_size) do |i|
       {:bitstring => random_bitstring(problem_size*BITS_PER_PARAM)}
     end
