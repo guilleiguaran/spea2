d2c78e4b27b26ef76437f6247ca335bc3eca5c55
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-14 18:01:09 -0500

    Fixes for CSV output

--- pareto.m
+++ pareto.m
@@ -1,2 +1,2 @@
-x = csvread('pareto.csv');
-plot3(x(:,1),x(:,2),x(:,3));
+x = csvread('solutions/pareto10.csv');
+plot3(x(:,1),x(:,2),x(:,3), '.', 'MarkerSize', 15);
--- problem_setup_spea2.rb
+++ problem_setup_spea2.rb
@@ -3,29 +3,12 @@ require "./eval_context"
 require "./functions"
 require "./restrictions"
 require "./solutions"
+require "csv"
 
-def print_solution(context, vector, n)
-  context.generate_solution(vector)
-  csv = CSV.open("solution#{n}.csv", "wb")
-  csv << ["g"]
-  context.g.each do |gi|
-    gi.each do |ex|
-      csv << ex
-    end
-  end
-  csv << [""]
-  csv << ["q"]
-  context.q.each do |qi|
-    qi.each do |ex|
-      ex.each do |exi|
-        csv << exi
-      end
-    end
-  end
-  csv << [""]
-  csv << ["c"]
-  context.c.each do |ci|
-    csv << ci
+def print_solution(vectors, context, n)
+  csv = CSV.open("solutions/solutions#{n}.csv", "wb")
+  vectors.each do |vector|
+    context.generate_solution(vector)
   end
   csv.close
 end
@@ -35,7 +18,7 @@ context.load_variables("info.json")
 options = {
   :problem_size => 2,
   :search_space => Array.new(2) {|i| [0, 10] },
-  :max_gens => 1,
+  :max_gens => 50,
   :pop_size => 80,
   :archive_size => 40,
   :p_cross => 0.98
@@ -43,12 +26,13 @@ options = {
 
 (10..14).each do |n|
   context.n = n
-  csv = CSV.open("pareto#{n}.csv", "wb")
+  puts "n: #{n}"
+  csv = CSV.open("solutions/pareto#{n}.csv", "wb")
   spea2_instance = Spea2.new(context, options)
   archive = spea2_instance.search
-  archive.each do |x|
-    csv << x[:objectives].join(",").split(",")
-    print_solution(context, x[:vector], n)
-  end
+  objectives = archive.map{ |x| x[:objectives] }
+  objectives.each{ |o| csv << o }
+  vectors = archive.map{ |v| x[:vector] }
+  print_solutions(vectors, context, n)
   csv.close
 end
