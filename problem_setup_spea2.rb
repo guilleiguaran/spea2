a252d7f08500f4e66cee37d6df1d1be0e15c3275
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-15 04:33:48 -0500

    Fix tables

--- problem_setup_spea2.rb
+++ problem_setup_spea2.rb
@@ -5,48 +5,52 @@ require "./restrictions"
 require "./solutions"
 require "csv"
 
-def print_solution(vectors, context, n)
+def print_solutions(vectors, context, n)
   csv = CSV.open("solutions/solutions#{n}.csv", "wb", { col_sep: ";", force_quotes: true })
   vectors.each do |vector|
     context.generate_solution(vector)
-    csv << ["****** INICIO SOLUCION [#{vector.join(", ")}] ******"]
-    (0..context.j).each do |jc|
+    csv << ["SOLUCION [#{vector.join(", ")}]"]
+    (0...context.n - 1).each do |jc|
       csv << ["k = fase", "1", "2", "3", "4", "5", "6", "7", "8", "Cio", "PHIio"]
       csv << ["m = movimiento", "WBL", "EBT", "NBL", "SBT", "EBL", "WBT", "SBL", "NBT"]
-      (0..context.i).each do |ic|
-        rows = ["g#{i+1}#{j+1}m int##{i+1}"]
-        (0..context.m).each do |mc|
-          rows << [context.g[ic][jc][mc].to_s]
+      (0...context.i).each do |ic|
+        rows = ["g#{ic+1}#{jc+1}m int##{ic+1}"]
+        (0...context.m).each do |mc|
+          rows << [context.g[ic][jc][mc]]
         end
-        rows << [context.c[ic][jc].to_s]
-        rows << [context.p[ic][jc].to_s]
+        rows << [context.c[ic][jc]]
+        rows << [context.p[ic][jc]]
         csv << rows
       end
 
-      (0..context.i).each do |ic|
-        rows = ["X#{i+1}#{j+1}m int##{i+1}"]
-        (0..context.m).each do |mc|
-          rows << [context.x(ic, jc, mc).to_s]
+      (0...context.i).each do |ic|
+        rows = ["X#{ic+1}#{jc+1}m int##{ic+1}"]
+        (0...context.m).each do |mc|
+          rows << [context.x(ic, jc, mc)]
         end
         csv << rows
       end
 
-      (0..context.i).each do |ic|
-        rows = ["S#{i+1}#{j+1}m int##{i+1}"]
-        (0..context.m).each do |mc|
-          rows << [context.s[ic][mc].to_s]
+      (0...context.i).each do |ic|
+        rows = ["S#{ic+1}#{jc+1}m int##{ic+1}"]
+        (0...context.m).each do |mc|
+          rows << [context.s[ic][mc]]
         end
         csv << rows
       end
-      (0..context.i).each do |ic|
-        rows = ["q#{i+1}#{j+1}m int##{i+1}"]
-        (0..context.m).each do |mc|
-          rows << [(context.q[ic][jc][mc][0] + context.q[ic][jc][mc][1]).to_s]
+      (0...context.i).each do |ic|
+        rows = ["q#{ic+1}#{jc+1}m int##{ic+1}"]
+        (0...context.m).each do |mc|
+          rows << [(context.q[ic][jc][mc][0] + context.q[ic][jc][mc][1])]
         end
         csv << rows
       end
+      csv << [""]
+      csv << [""]
     end
-    csv << ["****** FIN SOLUCION ******"]
+    csv << [""]
+    csv << [""]
+    csv << [""]
   end
   csv.close
 end
@@ -56,7 +60,7 @@ context.load_variables("info.json")
 options = {
   :problem_size => 2,
   :search_space => Array.new(2) {|i| [0, 10] },
-  :max_gens => 50,
+  :max_gens => 1,
   :pop_size => 80,
   :archive_size => 40,
   :p_cross => 0.98
@@ -70,7 +74,7 @@ options = {
   archive = spea2_instance.search
   objectives = archive.map{ |x| x[:objectives] }
   objectives.each{ |o| csv << o }
-  vectors = archive.map{ |v| x[:vector] }
+  vectors = archive.map{ |v| v[:vector] }
   print_solutions(vectors, context, n)
   csv.close
 end
